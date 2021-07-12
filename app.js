c755feadc73286958795c301ccb500b0114079c5
Shawn Fox <foxeasttrojan06@gmail.com>
2021-07-12 19:37:05 -0400

    server setup

--- app.js
+++ app.js
@@ -6,38 +6,61 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const routes = require("./controller");
+const sequelize = require("./config/connection");
+
 
 var app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: 3600000
+  }
+};
+
+app.use(session(sess));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+app.use(compression());
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (req, res, next) {
+  if (!req.session) {
+    return next(new Error('Oh no')) //handle error
+  }
+  next() //otherwise continue
 });
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("running server");
+  });
+})
 
 module.exports = app;
