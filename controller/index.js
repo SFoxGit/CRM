4d695381215a8ef1bc3f8d9cd19994755833beac
Shawn Fox <foxeasttrojan06@gmail.com>
2021-07-11 23:40:39 -0400

    controller save

--- controller/index.js
+++ controller/index.js
@@ -4,10 +4,6 @@ const apiRoutes = require("./api");
 
 
 router.use('/api', apiRoutes);
-router.get("/testAPI", function(req, res, next) {
-  res.send("API is working properly");
-});
-
 
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
