a44d571cdbfa46c11a7c87d3b04bc1667cb00717
Shawn Fox <foxeasttrojan06@gmail.com>
2021-07-14 23:55:18 -0400

    header visible

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 
 
 export default function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
-      <Navbar.Brand className="brand" href="/">C R M</Navbar.Brand>
+      <Navbar.Brand className="brand" href="/" style={{color: "black"}}>C R M</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/about">Customers</Nav.Link>
+          <Nav.Link href="/about" style={{color: "black"}}>Customers</Nav.Link>
           {/* <NavDropdown title="Work" id="collasible-nav-dropdown">
             <NavDropdown.Item href="/work">All Projects</NavDropdown.Item>
           </NavDropdown> */}
-          <Nav.Link href="/resume">Login</Nav.Link>
+          <Nav.Link href="/resume" style={{color: "black"}}>Login</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
