50e9a62a0a928b9633d8b186f6ac0ac461b9ce75
Shawn Fox <foxeasttrojan06@gmail.com>
2021-07-28 23:38:16 -0400

    contact model

---
+++ models/Contact.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Contact extends Model {}
+
+Contact.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "contact",
+  }
+);
+
+module.exports = Contact
