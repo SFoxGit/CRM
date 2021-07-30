176f5f246a8bf6732fe7a6b384b49b801da0cc7a
Shawn Fox <foxeasttrojan06@gmail.com>
2021-07-30 19:26:05 -0400

    add contact to db

---
+++ client/src/components/Contact/add.contact.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react'
+import Button from 'react-bootstrap/Button'
+import axios from 'axios';
+// import { useHistory } from "react-router";
+
+export default function AddContact(props) {
+  // const history = useHistory();
+  const nameRef = useRef();
+  const titleRef = useRef();
+  const phoneRef = useRef();
+  const id = props.id
+  const submit = async (e) => {
+    e.preventDefault();
+    await axios.post('/api/contact', {
+      name: nameRef.current.value, 
+      phone: phoneRef.current.value,  
+      title: titleRef.current.value,
+      company: id,
+    })
+    .then(res => {
+      console.log("added")
+      
+    })
+    .catch(err => console.log(err))
+  }
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label>Name</label>
+          <input ref={nameRef} type="text" className="form-control" aria-describedby="" />
+        </div>
+        <div className="form-group">
+          <label>Phone</label>
+          <input type="tel" ref={phoneRef} name="phone"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            required />
+          <small>Format: 123-456-7890</small>
+        </div>
+        <div className="form-group">
+          <label>Title</label>
+          <input ref={titleRef} type="text" className="form-control" aria-describedby="" />
+        </div>
+        <div className="btn-parent">
+          <Button onClick={submit} type="submit" className="btn">Submit</Button>
+        </div>
+      </form>
+    </div>
+  )
+}
--- client/src/components/Customers/detail.customer.jsx
+++ client/src/components/Customers/detail.customer.jsx
@@ -1,17 +1,19 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
+import AddContact from '../Contact/add.contact'
 
 export default function CustomerDetail(props) {
   const customer = props.customer
+  const [showForm, setShowForm] = useState(false)
   // const userID = props.userID
   const [custData, setCustData] = useState([])
   useEffect(() => {
     axios.get(`/api/customer/${customer}`)
-    .then(res => {
-      setCustData(res.data)
-    })
-    .catch(err => console.log(err))
+      .then(res => {
+        setCustData(res.data)
+      })
+      .catch(err => console.log(err))
   }, [customer])
   return (
     <Container>
@@ -23,7 +25,11 @@ export default function CustomerDetail(props) {
         <Col>{custData.industry}</Col>
         <Col>{custData.phone}</Col>
       </Row>
-      <Button>Add Contact</Button>
+      {showForm ?
+        <AddContact id={custData.id} />
+        :
+        <Button onClick={() => setShowForm(true)}>Add Contact</Button>
+      }
     </Container>
   )
 }
