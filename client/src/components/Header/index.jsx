00882890d3e0245d45456dcb11b31c69f170b6bb
Shawn Fox <foxeasttrojan06@gmail.com>
2021-07-30 21:55:19 -0400

    pointer on home

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
+import { useHistory } from "react-router-dom";
 
 
 export default function Header() {
+  const history = useHistory()
+
   return (
     <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
-      <Navbar.Brand className="brand" href="/" style={{color: "black"}}>C R M</Navbar.Brand>
+      <Navbar.Brand className="brand" onClick={() => history.push("/")} style={{color: "black", cursor: "pointer"}}>C R M</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/customers" style={{color: "black"}}>Customers</Nav.Link>
+          <Nav.Link onClick={() => history.push("/customers")} style={{color: "black" }}>Customers</Nav.Link>
           {/* <NavDropdown title="Work" id="collasible-nav-dropdown">
             <NavDropdown.Item href="/work">All Projects</NavDropdown.Item>
           </NavDropdown> */}
-          <Nav.Link href="/login" style={{color: "black"}}>Login</Nav.Link>
+          <Nav.Link onClick={() => history.push("/login")} style={{color: "black"}}>Login</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
